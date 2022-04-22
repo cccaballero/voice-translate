019acb03a1a4c87e680f318828a1275189f7d155
Carlos Cesar Caballero <ccesar@daxslab.com>
2022-04-22 19:29:17 -0400

    code formatting and structure improved

--- main-vosk-argos.py
+++ main-vosk-argos.py
@@ -10,67 +10,72 @@ import sys
 
 from argostranslate import package, translate
 
-stop_listening = False
 
-q = queue.Queue()
+def main():
+    stop_listening = False
 
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if not stop_listening:
-        if status:
-            print(status, file=sys.stderr)
-        q.put(bytes(indata))
+    q = queue.Queue()
 
+    def callback(indata, frames, time, status):
+        """This is called (from a separate thread) for each audio block."""
+        if not stop_listening:
+            if status:
+                print(status, file=sys.stderr)
+            q.put(bytes(indata))
 
-# argos
-if not os.path.exists("translate-es_en-1_0.argosmodel"):
-    print ("Please download an einglish-spanish translation model from https://www.argosopentech.com/argospm/index/")
-    print ("and unpack as 'translate-es_en-1_0.argosmodel' in the current folder.")
-    sys.exit(2)
-package.install_from_path('translate-es_en-1_0.argosmodel')
-installed_languages = translate.get_installed_languages()
-translation_es_en = installed_languages[1].get_translation(installed_languages[0])
-
-
-try:
-    if not os.path.exists("model"):
-        print ("Please download a model for your language from https://alphacephei.com/vosk/models")
-        print ("and unpack as 'model' in the current folder.")
+    # argos
+    if not os.path.exists("translate-es_en-1_0.argosmodel"):
+        print ("Please download an einglish-spanish translation model from https://www.argosopentech.com/argospm/index/")
+        print ("and unpack as 'translate-es_en-1_0.argosmodel' in the current folder.")
         sys.exit(2)
+    package.install_from_path('translate-es_en-1_0.argosmodel')
+    installed_languages = translate.get_installed_languages()
+    translation_es_en = installed_languages[1].get_translation(installed_languages[0])
 
-    device_info = sd.query_devices(None, 'input')
-    # soundfile expects an int, sounddevice provides a float:
-    samplerate = int(device_info['default_samplerate'])
 
-    model = vosk.Model('model')
+    try:
+        if not os.path.exists("model"):
+            print ("Please download a model for your language from https://alphacephei.com/vosk/models")
+            print ("and unpack as 'model' in the current folder.")
+            sys.exit(2)
 
-    with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=None, dtype='int16',
-                            channels=1, callback=callback):
-            print('#' * 80)
-            print('Press Ctrl+C to stop the recording')
-            print('#' * 80)
+        device_info = sd.query_devices(None, 'input')
+        # soundfile expects an int, sounddevice provides a float:
+        samplerate = int(device_info['default_samplerate'])
 
-            rec = vosk.KaldiRecognizer(model, samplerate)
-            while True:
-                data = q.get()
-                if rec.AcceptWaveform(data):
-                    result = json.loads(rec.Result())
-                    if result['text']:
-                        print('')
-                        print('#' * 10, ' Text ', '#' * 10)
-                        print('Text: ', result['text'])
-                        translated = translation_es_en.translate(result['text'])
-                        print('Translation: ', translated)
-                else:
-                    partial_result = json.loads(rec.PartialResult())
-                    if partial_result['partial']:
-                        print('')
-                        print('#' * 10, ' Partial ', '#' * 10)
-                        print('Text: ', partial_result['partial'])
-                        print('Translation: ', translation_es_en.translate(partial_result['partial']))
+        model = vosk.Model('model')
 
-except KeyboardInterrupt:
-    print('\nDone')
-except Exception as e:
-    print(type(e).__name__ + ': ' + str(e))
-    sys.exit(1)
+        with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=None, dtype='int16',
+                                channels=1, callback=callback):
+                print('#' * 80)
+                print('Press Ctrl+C to stop the recording')
+                print('#' * 80)
+
+                rec = vosk.KaldiRecognizer(model, samplerate)
+                while True:
+                    data = q.get()
+                    if rec.AcceptWaveform(data):
+                        result = json.loads(rec.Result())
+                        if result['text']:
+                            print('')
+                            print('#' * 10, ' Text ', '#' * 10)
+                            print('Text: ', result['text'])
+                            translated = translation_es_en.translate(result['text'])
+                            print('Translation: ', translated)
+                    else:
+                        partial_result = json.loads(rec.PartialResult())
+                        if partial_result['partial']:
+                            print('')
+                            print('#' * 10, ' Partial ', '#' * 10)
+                            print('Text: ', partial_result['partial'])
+                            print('Translation: ', translation_es_en.translate(partial_result['partial']))
+
+    except KeyboardInterrupt:
+        print('\nDone')
+    except Exception as e:
+        print(type(e).__name__ + ': ' + str(e))
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
--- main-vosk.py
+++ main-vosk.py
@@ -8,54 +8,60 @@ import sounddevice as sd
 import vosk
 import sys
 
-stop_listening = False
 
-q = queue.Queue()
+def main():
+    stop_listening = False
 
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if not stop_listening:
-        if status:
-            print(status, file=sys.stderr)
-        q.put(bytes(indata))
+    q = queue.Queue()
+
+    def callback(indata, frames, time, status):
+        """This is called (from a separate thread) for each audio block."""
+        if not stop_listening:
+            if status:
+                print(status, file=sys.stderr)
+            q.put(bytes(indata))
 
 
-try:
-    if not os.path.exists("model"):
-        print ("Please download a model for your language from https://alphacephei.com/vosk/models")
-        print ("and unpack as 'model' in the current folder.")
-        sys.exit(2)
+    try:
+        if not os.path.exists("model"):
+            print ("Please download a model for your language from https://alphacephei.com/vosk/models")
+            print ("and unpack as 'model' in the current folder.")
+            sys.exit(2)
 
-    device_info = sd.query_devices(None, 'input')
-    # soundfile expects an int, sounddevice provides a float:
-    samplerate = int(device_info['default_samplerate'])
+        device_info = sd.query_devices(None, 'input')
+        # soundfile expects an int, sounddevice provides a float:
+        samplerate = int(device_info['default_samplerate'])
 
-    model = vosk.Model('model')
+        model = vosk.Model('model')
 
-    with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=None, dtype='int16',
-                            channels=1, callback=callback):
-            print('#' * 80)
-            print('Press Ctrl+C to stop the recording')
-            print('#' * 80)
+        with sd.RawInputStream(samplerate=samplerate, blocksize = 8000, device=None, dtype='int16',
+                                channels=1, callback=callback):
+                print('#' * 80)
+                print('Press Ctrl+C to stop the recording')
+                print('#' * 80)
 
-            rec = vosk.KaldiRecognizer(model, samplerate)
-            while True:
-                data = q.get()
-                if rec.AcceptWaveform(data):
-                    result = json.loads(rec.Result())
-                    if result['text']:
-                        print('')
-                        print('#' * 10, ' Text ', '#' * 10)
-                        print('Text: ', result['text'])
-                else:
-                    partial_result = json.loads(rec.PartialResult())
-                    if partial_result['partial']:
-                        print('')
-                        print('#' * 10, ' Partial ', '#' * 10)
-                        print('Text: ', partial_result['partial'])
+                rec = vosk.KaldiRecognizer(model, samplerate)
+                while True:
+                    data = q.get()
+                    if rec.AcceptWaveform(data):
+                        result = json.loads(rec.Result())
+                        if result['text']:
+                            print('')
+                            print('#' * 10, ' Text ', '#' * 10)
+                            print('Text: ', result['text'])
+                    else:
+                        partial_result = json.loads(rec.PartialResult())
+                        if partial_result['partial']:
+                            print('')
+                            print('#' * 10, ' Partial ', '#' * 10)
+                            print('Text: ', partial_result['partial'])
 
-except KeyboardInterrupt:
-    print('\nDone')
-except Exception as e:
-    print(type(e).__name__ + ': ' + str(e))
-    sys.exit(1)
+    except KeyboardInterrupt:
+        print('\nDone')
+    except Exception as e:
+        print(type(e).__name__ + ': ' + str(e))
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
